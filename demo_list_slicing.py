e2a37633693bea8d18a6b2affb88d1cc1470bc5c
Devesh Kumar <deyvesshkumar@gmail.com>
2025-05-02 20:09:48 +0530

    adding demo_list_slicing.py

---
+++ demo_list_slicing.py
@@ -0,0 +1,10 @@
+
+
+original_list = list(range(1, 11))
+print('Original list:', original_list)
+
+first_five_elements = (original_list[:5])
+print('Extracted first five elements:', first_five_elements)
+
+reverse_first_five_elements = first_five_elements[::-1]
+print('Reversed first five elements:', reverse_first_five_elements)
